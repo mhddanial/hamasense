9c8e1c71c1b9ecfa268e31f7fa726db07b6f2864
bastian <bastian160305@gmail.com>
2025-12-14 14:05:26 +0700

    memperbaiki route pada admin

--- laravel/routes/admin.php
+++ laravel/routes/admin.php
@@ -4,25 +4,27 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AdminController;
 use App\Http\Controllers\ArticleCategoryController;
 use App\Http\Controllers\ArticleController;
+use App\Http\Controllers\DiseaseController;
 use App\Http\Controllers\PestController;
 use App\Http\Controllers\PlantTypeController;
 use Inertia\Inertia;
 
-Route::prefix('admin')->group(function () {
-    Route::get('/', [AdminController::class, 'dashboard']);
+Route::middleware('admin')->prefix('admin')->group(function () {
+    Route::get('/dashboard', [AdminController::class, 'dashboard']);
 
     Route::resource('/pest', PestController::class);
     Route::resource('/plant', PlantTypeController::class);
     Route::resource('/article', ArticleController::class);
     Route::resource('/article-category', ArticleCategoryController::class);
+    Route::resource('/disease', DiseaseController::class);
 });
 
 Route::get('/user', [AdminController::class, 'user']);
 
-Route::prefix('/api')->group(function () {
-    Route::apiResource('/article', ArticleController::class);
+// Route::prefix('/api')->group(function () {
+//     Route::apiResource('/article', ArticleController::class);
 
-    Route::apiResource('/article_category', ArticleCategoryController::class);
-    Route::apiResource('/pest', PestController::class);
+//     Route::apiResource('/article_category', ArticleCategoryController::class);
+//     Route::apiResource('/pest', PestController::class);
 
-});
+// });
